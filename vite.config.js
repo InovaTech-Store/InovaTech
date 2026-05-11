eef5afe60f115b4fbf67dd88f803cdf7b7f2fe8f
samuel <samuel.vcosta@al.infnet.edu.br>
2026-05-11 16:20:07 -0300

    ajustes finais packjason

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
     // Substitua 'nome-do-seu-repositorio' pelo nome real do seu projeto no GitHub
-    base: command === 'build' ? '/InovaTech/' : '/',
+    base: './',
   }
 })
